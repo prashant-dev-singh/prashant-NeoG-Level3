69290e6231939b7c7fddba1fb407b2fbc45d0c6e
prashant-dev-singh <74811257+prashant-dev-singh@users.noreply.github.com>
2020-12-19 17:53:43 +0530

    commit

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 
-//var name= prompt("Username: ");
-//alert("Hi ! Welcome Mr. " +name);
-
-
-
 //var btnShowMessage = document.getElementById('btnClick'); 
 var btnShowMessage = document.querySelector('#btnClick'); 
 var txtInput = document.querySelector('#txtInput');
 var txtOutput = document.querySelector("#txtOutput");
-
-
 
 
 btnShowMessage.addEventListener('click',function showMessage(){
@@ -19,10 +12,10 @@ var inputText = txtInput.value;
 
  //var tempUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=" + inputText;
 
- // var tempUrl = "https://api.funtranslations.com/translate/pirate.json" +"?text=" + inputText;
+  var tempUrl = "https://api.funtranslations.com/translate/minion.json" +"?text=" + inputText;
 // Pirate API
 
- var tempUrl = "https://api.funtranslations.com/translate/cockney.json" +"?text=" + inputText;
+ //var tempUrl = "https://api.funtranslations.com/translate/cockney.json" +"?text=" + inputText;
  console.log(tempUrl);
 
  
